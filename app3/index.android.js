9d074952afb3ab021172871a398ac2ada193e971
Renato <renatovalerio25@gmail.com>
2017-05-17 13:04:41 -0300

    Iniciando app jokenpo e implementando logica state

---
+++ app3/index.android.js
@@ -0,0 +1,42 @@
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button
+} from 'react-native';
+
+
+
+class app3 extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { escolhaUsuario: '' }
+
+  }
+  jokenpo(escolhaUsuario) {
+    this.setState({ escolhaUsuario: escolhaUsuario });
+  }
+
+
+
+  render() {
+    return (
+      <View>
+        <Text>Escolha do Computador</Text>
+        <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
+        <Text>Resultado</Text>
+        <Button title='pedra' onPress={() => { this.jokenpo('pedra') }} />
+        <Button title='papel' onPress={() => { this.jokenpo('papel') }} />
+        <Button title='tesoura' onPress={() => { this.jokenpo('tesoura') }} />
+
+      </View>
+    )
+  }
+}
+
+AppRegistry.registerComponent('app3', () => app3);
