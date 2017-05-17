b633c0c49805d08255f7232595eeadc0c3df8207
Renato <renatovalerio25@gmail.com>
2017-05-17 19:54:06 -0300

    Refatorando codigo e adicionando imagens

--- app3/index.android.js
+++ app3/index.android.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  Image
 } from 'react-native';
 
 
@@ -79,16 +80,72 @@ class app3 extends Component {
   render() {
     return (
       <View>
-        <Text>Escolha do Computador: {this.state.escolhaCompuador}</Text>
-        <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
-        <Text>Resultado: {this.state.resultado}</Text>
-        <Button title='pedra' onPress={() => { this.jokenpo('pedra') }} />
-        <Button title='papel' onPress={() => { this.jokenpo('papel') }} />
-        <Button title='tesoura' onPress={() => { this.jokenpo('tesoura') }} />
+        <Topo></Topo>
+
+        <View style={styles.painelAcoes}>
+
+          <View style={styles.btnEscolha}>
+            <Button title='pedra' onPress={() => { this.jokenpo('pedra') }} />
+          </View>
+
+          <View style={styles.btnEscolha}>
+            <Button title='papel' onPress={() => { this.jokenpo('papel') }} />
+          </View>
+
+          <View style={styles.btnEscolha}>
+            <Button title='tesoura' onPress={() => { this.jokenpo('tesoura') }} />
+          </View>
+
+        </View>
+
+        <View style={styles.palco}>
+
+          <Text style={styles.txtResultado}>{this.state.resultado}</Text>
+          <Text>Escolha do Computador: {this.state.escolhaCompuador}</Text>
+          <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
+
+        </View>
+
 
       </View>
     )
   }
 }
+
+class Topo extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('./imgs/jokenpo.png')}
+        // style={{ flex: 1, resizeMode: 'stretch' }}
+        />
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  btnEscolha: {
+    width: 90,
+
+  },
+  painelAcoes: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+
+  },
+  palco: {
+    alignItems: 'center',
+    marginTop: 10
+
+  },
+  txtResultado: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'red',
+    height: 60
+  }
+});
 
 AppRegistry.registerComponent('app3', () => app3);
